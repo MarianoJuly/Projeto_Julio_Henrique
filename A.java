98feff3201caa4bbaff58517b6f449ee6cad5a94
MarianoJuly <marcianocruzeiri@gmail.com>
2023-09-26 01:51:32 -0300

    mudança de classes

---
+++ A.java
@@ -0,0 +1,26 @@
+public class A{
+    private int A1;
+    private float A2;
+
+    public A(){}
+
+    public void setA1(int x){
+        this.A1 = x;
+    }
+
+    public void setA2(float y){
+        this.A2 = y;
+    }
+
+    public int getA1(){
+        return this.A1;
+    }
+    
+    public float getA2(){
+        return this.A2;
+    }
+
+    
+
+
+}
---
+++ B.java
@@ -0,0 +1,34 @@
+public class B{
+    private int B1;
+    private float B2;
+
+
+    public B(){}
+
+    public void setB1(int x){
+        this.B1 = x;
+    }
+
+    public void setB2(float y){
+        this.B2 = y;
+    }
+
+    public int getB1(){
+        return this.B1;
+    }
+    
+    public float getB2(){
+        return this.B2;
+    }
+
+    public void MB1(){
+        System.out.println("Teste B");
+    }
+    
+    public void MB2(){
+        System.out.println("Teste B, mas metodo 2");
+    }
+
+
+}
+
